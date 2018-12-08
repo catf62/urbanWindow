d1e4d578d586c72564d04290c3adf2af582bdfc4
Ross Hancock <rosshancock91@gmail.com>
2018-12-08 11:29:46 +0000

    image url added to the seed api script

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,24 +3,20 @@ const RequestHelper = require('../helpers/request_helpers.js')
 const Seeds = function () {
   this.cities = [];
   this.cityNames = [];
-  this.cityNamesUrl = 'https://api.teleport.org/api/urban_areas';
+  this.cityUrl = 'https://api.teleport.org/api/urban_areas';
 };
 
-
-
-
 Seeds.prototype.getData = function () {
-  const cityNameRequest = new RequestHelper(this.cityNamesUrl);
+  const cityNameRequest = new RequestHelper(this.cityUrl);  // get request for cities
   cityNameRequest.get()
   .then((citiesResponse) => {
-    this.extractCityNames(citiesResponse);
-    return citiesResponse['_links']['ua:item'];
+    this.extractCityNames(citiesResponse); // save names to this.cityNames and creates a city object in this.cities
+    return citiesResponse['_links']['ua:item']; // return array of cities - names and link
   })
   .then((cities) => {
-
-  this.getPicture(cities)
+    return this.getPicture(cities) // save picture url to the city in this.cities
   })
-.catch(console.error);
+  .catch(console.error);
 };
 
 Seeds.prototype.extractCityNames = function (response) {
@@ -30,19 +26,24 @@ Seeds.prototype.extractCityNames = function (response) {
     let cityObject = {'name': city['name']}
     this.cities.push(cityObject)
   });
-
 }
 
 Seeds.prototype.getPicture = function (cities) {
-
+// cities is an array of city name and url
   cities.forEach((city) => {
-
-    const pictureRequest = new RequestHelper(city['href'] + `images`)
-    console.log(city['href'] + `images`);
     let cityPictureURL;
+    const pictureRequest = new RequestHelper(city['href'] + `images`) // get request of the city image url
     pictureRequest.get()
     .then((pictureObject) => {
-      this.extractPicture(pictureObject)
+      cityPictureURL = this.extractPicture(pictureObject);
+      // find the city in this.cities and add cityPictureURL
+      this.addPictureURL(city, cityPictureURL);
+      // find the index of the objects with the city name
+
+      // add a new key value pair to the object
+    })
+    .then((response) => {
+      console.log(this.cities);
     })
     .catch(console.error);
   });
@@ -50,7 +51,17 @@ Seeds.prototype.getPicture = function (cities) {
 
 Seeds.prototype.extractPicture = function (pictureObject) {
   const pictureUrl = pictureObject['photos'][0]['image']['web'];
-  cityPictureURL = pictureUrl;
+  return pictureUrl;
 };
+
+Seeds.prototype.addPictureURL = function (searchCity, cityPictureURL) {
+  const index = this.cities.findIndex((cityInCities) => {
+    return cityInCities['name'] === searchCity['name']
+  })
+  console.log(index);
+  const selectedCity = this.cities[index];
+  selectedCity.pictureUrl = cityPictureURL;
+};
+
 
 module.exports = Seeds;
