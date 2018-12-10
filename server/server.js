9b41562cd5061e4dbbfb99103539c44120a1464f
Ross Hancock <rosshancock91@gmail.com>
2018-12-10 10:49:02 +0000

    server added with GET BY ID route

--- server/server.js
+++ server/server.js
@@ -1,93 +1,22 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
 
 const publicPath = path.join(__dirname, '../client/public');
 app.use(express.static(publicPath));
+app.use(parser.json())
 
-const teas = [
-  { city_name: "Aarhus",
-    image_url: "https://d13k13wj6adfdf.cloudfront.net/urban_areas/aarhus_web-1462c370b8.jpg",
-    categories: [
-      {
-        color: "#f3c32c",
-        name: "Housing",
-        score_out_of_10: "6.1315"
-      },
-      {
-        color: "#f3d630",
-        name: "Cost of Living",
-        score_out_of_10: "4.015"
-      },
-      {
-        color: "#f4eb33",
-        name: "Startups",
-        score_out_of_10: "2.827"
-      }
-    ]},
-  { city_name: "Adelaide",
-    image_url: "https://d13k13wj6adfdf.cloudfront.net/urban_areas/adelaide_web-e462ed5b74.jpg",
-    categories: [
-      {
-        color: "#f3c32c",
-        name: "Housing",
-        score_out_of_10: "6.3095"
-      },
-      {
-        color: "#f3d630",
-        name: "Cost of Living",
-        score_out_of_10: "4.692"
-      },
-      {
-        color: "#f4eb33",
-        name: "Startups",
-        score_out_of_10: "3.1365"
-      }
-    ] },
-  { city_name: "Albuquerque",
-    image_url: "https://d13k13wj6adfdf.cloudfront.net/urban_areas/albuquerque_web-3079b54e59.jpg",
-    categories: [
-      {
-        color: "#f3c32c",
-        name: "Housing",
-        score_out_of_10: "7.262"
-      },
-      {
-        color: "#f3d630",
-        name: "Cost of Living",
-        score_out_of_10: "6.059"
-      },
-      {
-        color: "#f4eb33",
-        name: "Startups",
-        score_out_of_10: "3.772"
-      }
-    ] },
-  { city_name: "Almaty",
-    image_url: "https://d13k13wj6adfdf.cloudfront.net/urban_areas/almaty_web-acac42d539.jpg",
-    categories: [
-      {
-        color: "#f3c32c",
-        name: "Housing",
-        score_out_of_10: "9.282"
-      },
-      {
-        color: "#f3d630",
-        name: "Cost of Living",
-        score_out_of_10: "9.333"
-      },
-      {
-        color: "#f4eb33",
-        name: "Startups",
-        score_out_of_10: "2.4585"
-      }
-    ] }
-];
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+    const db = client.db('cities');
+    const citiesCollection = db.collection('cities');
+    const citiesRouter = createRouter(citiesCollection);
+    app.use('/api/cities', citiesRouter);
+  });
 
 app.listen(3000, function () {
   console.log(`App running on port ${ this.address().port }`);
-});
-
-app.get('/api/cities', (req, res) => { // NEW
-  res.json(teas);
 });
